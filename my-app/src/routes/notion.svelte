961ec355267a06ef5dfc836ac888dc8f6ec5411b
Chandan Mahapatra <chandan.m1992@gmail.com>
2025-02-03 00:32:22 +0000

    updates to deploy

---
+++ my-app/src/routes/notion.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { onMount } from 'svelte';
+	let notionContent = '';
+
+	onMount(async () => {
+		const res = await fetch('/notion-content/page.md');
+		notionContent = await res.text();
+	});
+</script>
+
+<main>
+	<h1>Notion Page Content</h1>
+	<pre>{notionContent}</pre>
+</main>
